8ca0922c35af9256783b8a75d74c1a80f5c922df
Jorge Ruiz <jorgeruiz@atbim.es>
2022-09-22 12:00:56 +0200

    creacion de perfil

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT } from './types';
+import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT, CLEAR_PROFILE } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
 // Load User
@@ -91,4 +91,5 @@ export const login = (email, password) => async dispatch => {
 // Logout / Clear Profile
 export const logout = () => dispatch => {
     dispatch({ type: LOGOUT });
+    dispatch({ type: CLEAR_PROFILE });
 }
