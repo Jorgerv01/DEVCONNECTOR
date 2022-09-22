5d4a3d5aff743b9de9666a4c88a85c0cbc313bac
Jorge Ruiz <jorgeruiz@atbim.es>
2022-09-22 09:13:04 +0200

    log in, logout y register por parte del cliente

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR } from './types';
+import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
 // Load User
@@ -40,6 +40,8 @@ export const register = ({ name, email, password }) => async dispatch => {
             type: REGISTER_SUCCESS,
             payload: res.data
         });
+        dispatch(loadUser());
+
     } catch (err) {
         const errors = err.response.data.errors;
         
@@ -52,4 +54,41 @@ export const register = ({ name, email, password }) => async dispatch => {
         });
         
     }
+};
+
+// Login User
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({email, password });
+
+    try {
+        const res = await axios.post('http://localhost:5000/api/auth', body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+        
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: LOGIN_FAIL
+        });
+        
+    }
+};
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
 }
