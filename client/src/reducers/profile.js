246ac44c39f109ffe84375d5bf7fad1b6a7f2a89
Jorge Ruiz <jorgeruiz@atbim.es>
2022-09-23 10:25:04 +0200

    add experience and education

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
 
 const initialState = {
     profile: null,
@@ -20,6 +20,7 @@ export default function(state = initialState, action){
                 loading: false
             }
         case GET_PROFILE:
+        case UPDATE_PROFILE:
             return {
                 ...state,
                 profile: payload,
