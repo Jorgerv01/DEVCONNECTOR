c77d0ad456f64228504ced1429f2e73788beb879
Jorge Ruiz <jorgeruiz@atbim.es>
2022-09-23 09:06:15 +0200

    edicion de perfiles

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import { Link } from 'react-router-dom';
+import DashboardActions from './DashboardActions';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
@@ -14,7 +15,11 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
     <h1 className='large text-primary'>Dashboard</h1>
     <p className='lead'>
     <i className='fas fa-user'></i>Welcome { user && user.name }</p>
-    {profile !== null ? (<Fragment>has</Fragment>) : (
+    {profile !== null ? 
+    (
+    <Fragment>
+      <DashboardActions />
+    </Fragment>) : (
     <Fragment>
       <p>You have not yet setup a profile, please add some info</p>
       <Link to='/create-profile' className="btn btn-primary my-1">
