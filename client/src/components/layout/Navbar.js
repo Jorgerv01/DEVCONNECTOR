6453bbd29c1778f2897a436956ae5c49594a55b1
Jorge Ruiz <jorgeruiz@atbim.es>
2022-10-11 11:58:48 +0200

    posts in front

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,6 +14,12 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
       </li>
       <li>
+        <Link to='/posts'>
+          Posts
+        </Link>
+
+      </li>
+      <li>
         <Link to='/dashboard'><i className="fas fa-user" />{' '}
           <span className="hide-sm"> Dashboard</span></Link>
 
